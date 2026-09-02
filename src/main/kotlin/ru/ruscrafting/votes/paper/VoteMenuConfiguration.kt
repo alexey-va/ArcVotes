8c3187b0165fd51470cc306c669af833327971f7
alexey-va <freedeeml@gmail.com>
2026-09-02 16:47:31 +0300

    refactor(gui): share atomic menu configuration parser

--- src/main/kotlin/ru/ruscrafting/votes/paper/VoteMenuConfiguration.kt
+++ src/main/kotlin/ru/ruscrafting/votes/paper/VoteMenuConfiguration.kt
@@ -3,9 +3,8 @@ package ru.ruscrafting.votes.paper
 import ru.arc.config.Config
 import ru.arc.config.ConfigManager
 import ru.arc.menu.MenuCatalog
-import ru.arc.menu.MenuLayoutParser
 import ru.arc.paper.menu.PaperMenuItemTemplate
-import ru.arc.paper.menu.PaperMenuItemTemplateParser
+import ru.arc.paper.menu.PaperMenuConfigurationParser
 import ru.ruscrafting.votes.config.VoteMenuSchema
 import java.nio.file.Path
 
@@ -19,16 +18,14 @@ data class VoteMenuConfiguration(
         fun loadFresh(dataRoot: Path): VoteMenuConfiguration = parse(Config(dataRoot, "config.yml"))
 
         private fun parse(config: Config): VoteMenuConfiguration {
-            val catalog = MenuLayoutParser.require(config, "gui.layouts", VoteMenuSchema.contracts)
-            val templates = PaperMenuItemTemplateParser.require(config, "gui.templates")
-            val referenced = catalog.layouts.values.flatMap { layout ->
-                listOfNotNull(layout.backgroundTemplate?.value) +
-                    layout.elements.values.mapNotNull { it.template?.value }
-            }.toSet()
-            val missing = referenced - templates.keys
-            require(missing.isEmpty()) { "GUI layouts reference missing templates: ${missing.sorted()}" }
-            require("loading" in templates) { "GUI template 'loading' is required" }
-            return VoteMenuConfiguration(catalog, templates)
+            val configuration = PaperMenuConfigurationParser.require(
+                config,
+                "gui.layouts",
+                "gui.templates",
+                VoteMenuSchema.contracts,
+                requiredTemplates = setOf("loading"),
+            )
+            return VoteMenuConfiguration(configuration.catalog, configuration.templates)
         }
     }
 }
